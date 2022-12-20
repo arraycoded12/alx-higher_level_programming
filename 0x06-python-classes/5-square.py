23e1dbbeef3aff4d461f3209162fb0a8eb421c8b
Adelayo Gbolahan <111052542+arraycoded12@users.noreply.github.com>
2022-12-20 22:36:28 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,107 +1,54 @@
 #!/usr/bin/python3
-
 # 0-square.py by Ehoneah Obed
-
 """A module that defines a square """
 
 
-
-
-
 class Square:
+    """A class that represents a square"""
 
-        """A class that represents a square"""
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
 
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
 
+        self.__size = size
 
-            def __init__(self, size=0):
+    @property
+    def size(self):
+        """Retrieves size of square"""
 
-                        """Initializing this square class
+        return self.__size
 
-                                Args:
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
-                                            size: represnets the size of the square defined
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
 
-                                                    Raises:
+        return (self.__size ** 2)
 
-                                                                TypeError: if size is not integer
+    def my_print(self):
+        """print the square in # """
 
-                                                                            ValueError: if size is less than zero
+        if self.__size == 0:
+            print()
 
-                                                                                    """
-
-
-
-                                                                                            if not isinstance(size, int):
-
-                                                                                                            raise TypeError('size must be an integer')
-
-                                                                                                                if size < 0:
-
-                                                                                                                                raise ValueError('size must be >= 0')
-
-
-
-                                                                                                                                    self.__size = size
-
-
-
-                                                                                                                                        @property
-
-                                                                                                                                            def size(self):
-
-                                                                                                                                                        """Retrieves size of square"""
-
-
-
-                                                                                                                                                                return self.__size
-
-
-
-                                                                                                                                                                @size.setter
-
-                                                                                                                                                                    def size(self, value):
-
-                                                                                                                                                                                if not isinstance(value, int):
-
-                                                                                                                                                                                                raise TypeError('size must be an integer')
-
-                                                                                                                                                                                                    if value < 0:
-
-                                                                                                                                                                                                                    raise ValueError('size must be >= 0')
-
-                                                                                                                                                                                                                        self.__size = value
-
-
-
-                                                                                                                                                                                                                            def area(self):
-
-                                                                                                                                                                                                                                        """
-
-                                                                                                                                                                                                                                                Calculate area of the square
-
-                                                                                                                                                                                                                                                        Returns: The square of the size
-
-                                                                                                                                                                                                                                                                """
-
-
-
-                                                                                                                                                                                                                                                                        return (self.__size ** 2)
-
-
-
-                                                                                                                                                                                                                                                                        def my_print(self):
-
-                                                                                                                                                                                                                                                                                    """print the square in # """
-
-
-
-                                                                                                                                                                                                                                                                                            if self.__size == 0:
-
-                                                                                                                                                                                                                                                                                                            print()
-
-
-
-                                                                                                                                                                                                                                                                                                                    for i in range(self.__size):
-
-                                                                                                                                                                                                                                                                                                                                    print("#" * self.__size)
+        for i in range(self.__size):
+            print("#" * self.__size)
